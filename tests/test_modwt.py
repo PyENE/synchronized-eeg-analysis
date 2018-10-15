a9980b1dae97c954458f261783e3ae16fa6a8795
brice-olivier <briceolivier1409@gmail.com>
2018-10-15 17:09:51 +0200

    test modwt result

---
+++ tests/test_modwt.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Brice Olivier'
+
+import scipy.io
+from sea import MODWT
+import pytest
+
+
+@pytest.fixture(scope="module")
+def modwt_init():
+    eeg_data = scipy.io.loadmat('../data/s01_sample.mat', squeeze_me=True, struct_as_record=False)['EEG']
+    text_id = 0
+    channel_id = 0
+    return MODWT(eeg_data.data[channel_id, :, text_id], tmin=400,
+                 tmax=2000, margin=100, nlevels=7, wf='la8')
+
+
+def test_modwt(modwt_init):
+    assert modwt_init.wt.shape[0] == 7
+    assert modwt_init.wt.shape[1] == 1600
